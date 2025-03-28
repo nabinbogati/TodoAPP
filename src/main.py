5383b9f52daae54ea7d22f7dccd8b9c6e09d09cd
nabinbogati <nabin31bogati@gamil.com>
2025-03-28 10:18:55 +0545

    updated task package

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
+from auth.models import user_schema
+from auth.routes import auth_router
+from database import SqliteContext
 from fastapi import FastAPI
-from task.routes import router
+from task.models import task_schema
+from task.routes import task_router
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(task_router)
+app.include_router(auth_router)
+
+with SqliteContext() as cursor:
+    cursor.execute(task_schema)
+    cursor.execute(user_schema)
--- src/task/routes.py
+++ src/task/routes.py
@@ -2,18 +2,18 @@ import fastapi
 from fastapi import APIRouter, Response
 from fastapi.responses import JSONResponse
 
-from . import crud
-from .schema import Task
+from task import crud
+from task.schema import Task
 
-router = APIRouter()
+task_router = APIRouter()
 
 
-@router.get("/tasks", status_code=fastapi.status.HTTP_200_OK, response_model=list)
+@task_router.get("/tasks", status_code=fastapi.status.HTTP_200_OK, response_model=list)
 async def get_all_tasks():
     return crud.get_tasks()
 
 
-@router.get(
+@task_router.get(
     "/tasks/{task_id}",
     status_code=fastapi.status.HTTP_200_OK,
     response_model=Task | list,
@@ -22,17 +22,19 @@ async def get_single_task(task_id: int):
     return crud.get_tasks(task_id)
 
 
-@router.post("/tasks", status_code=fastapi.status.HTTP_201_CREATED, response_model=None)
+@task_router.post(
+    "/tasks", status_code=fastapi.status.HTTP_201_CREATED, response_model=None
+)
 async def create_tasks(tasks: list[Task]) -> Response:
     crud.add_tasks(tasks)
     return JSONResponse(content={"success": True})
 
 
-# @router.put("/tasks/{task_id}", status_code=fastapi.status.HTTP_200_OK)
+# @task_router.put("/tasks/{task_id}", status_code=fastapi.status.HTTP_200_OK)
 # async def replace_task(task_id: int, task: Task):
 #     pass
 
 
-# @router.patch("/tasks/{task_id}", status_code=fastapi.status.HTTP_200_OK)
+# @task_router.patch("/tasks/{task_id}", status_code=fastapi.status.HTTP_200_OK)
 # async def update_task(task_id: int, task: Task):
 #     pass
